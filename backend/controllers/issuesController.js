bf9bad3804e73d6bec2aed135ad7c52969295cb9
Sara Hamid <135767498+sahamid12@users.noreply.github.com>
2025-07-25 20:01:49 -0230

    Update issuesController.js

--- backend/controllers/issuesController.js
+++ backend/controllers/issuesController.js
@@ -1,10 +1,6 @@
 import pool from '../db.js';
 import { v2 as cloudinary } from 'cloudinary';
-cloudinary.config({
-    cloud_name: 'dguwxrhz2',
-    api_key: '519474269664854',
-    api_secret: 'n4USebzMTS59KPniSn_IjEqXx6g'
-  });
+
 
   
 export const getAllIssues = async (req, res) => {
