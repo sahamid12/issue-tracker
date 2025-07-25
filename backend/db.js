fc7cb6b1d279b3becc7879d78dbb6a40fd3a881c
Sara Hamid <135767498+sahamid12@users.noreply.github.com>
2025-07-25 20:01:17 -0230

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,1 @@
-import pkg from 'pg';
-const { Pool } = pkg;
 
-const pool = new Pool({
-  user: 'agency_user',               // DB username
-  host: 'localhost',                 // Host
-  database: 'agency_db',            // Your database name
-  password: 'CanadaAmerica12@#',       // Your password
-  port: 5432,                        // Default PostgreSQL port
-});
-
-export default pool;
